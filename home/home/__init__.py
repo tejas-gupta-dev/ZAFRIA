eca1b61f415efe391628701a4efd98f14ad477c8
tejas-gupta-dev <tejasgupta1512@gmail.com>
2025-07-06 03:00:12 +0530

    Update __init__.py

---
+++ home/home/__init__.py
@@ -0,0 +1,13 @@
+import os
+import django
+from django.core.management import call_command
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
+django.setup()
+
+# Run migrations automatically at startup
+try:
+    call_command('makemigrations', interactive=False)
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration error: {e}")
